db30db4e6de435268e5337db44e7ed3e7bf9919d
dimassulz <dimas.filho@df.docente.senai.br>
2022-06-11 11:54:12 -0300

    add code aula 1

---
+++ fundamentos/bloco_agrupador.js
@@ -0,0 +1,17 @@
+{
+    console.log("bloco 1 - sentenca 1");
+    console.log("bloco 1 - sentenca 2");
+    console.log("bloco 1 - sentenca 3");
+}
+
+{
+    {
+        {
+            {
+                console.log("bloco 2 - sentenca 1");
+                console.log("bloco 2 - sentenca 2");
+                console.log("bloco 2 - sentenca 3");
+            }
+        }
+    }
+}
---
+++ fundamentos/inicio.js
@@ -0,0 +1,14 @@
+// Estou escrevendo um comentario de linha
+
+console.log('Olá mundo esse é o primeiro teste');
+console.log("Escrevendo uma nova sentença!");
+
+/* 
+Estou 
+escrevendo um comentario 
+de multiplas linhas
+*/
+
+console.log("Ordem 1");
+console.log("Ordem 2");
+console.log("Ordem 3");
